fe180cd929ed0c1682d325838e129e7e886bf2ec
habo242 <habiel.abromand@googlemail.com>
2014-04-25 15:32:10 +0200

    erster versuch(keine Veränderung)

--- db/migrate/20140408102319_create_pkws.rb
+++ db/migrate/20140408102319_create_pkws.rb
@@ -1,12 +1,14 @@
 class CreatePkws < ActiveRecord::Migration
   def change
     create_table :pkws do |t|
+
       t.string :marke
       t.string :modell
       t.integer :kilometerstand
       t.boolean :tüv
 
       t.timestamps
+
     end
   end
 end
